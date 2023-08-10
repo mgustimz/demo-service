14d5aaf0fffad542d4b20ad363b8195cc99879a6
gustimaulana <gusti.maulana@advotics.com>
2023-08-10 08:40:23 +0700

    refactor(demo): remove unused import and class

--- src/test/java/com/example/demo/employee/usecase/create/CreateEmployeeTest.java
+++ src/test/java/com/example/demo/employee/usecase/create/CreateEmployeeTest.java
@@ -11,7 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.times;
 
 class CreateEmployeeTest {
 
